eff6527e125e1d1106cfbf4410e4221b349df141
srivatsan <vatsan_k@hotmail.com>
2022-03-20 15:05:55 +0530

    Third Update

--- docker-git-jenkins/src/main/java/com/example/demo/controllers/TeacherController.java
+++ docker-git-jenkins/src/main/java/com/example/demo/controllers/TeacherController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,4 +14,15 @@ public class TeacherController {
 		
 		return "Mala Bala Kala";
 	}
+	
+	@GetMapping(path = "/teachers/{id}")
+	public String getTeachersById(@PathVariable("id") int id) {
+		
+		String response ="Nila";
+		if(id==1) {
+		response ="Mala";
+		}
+		return response;
+	}
+	
 }
